9162fdfaf282adb1f35b915c806d94dce36421a3
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:15:42 +0700

    Update Bai108.java

--- Bai108.java
+++ Bai108.java
@@ -1,9 +1,20 @@
 package baitap;
 
 public class Bai108 {
+import java.util.Scanner;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.util.Scanner;
+
+     public static void main(String[] arg) {	
+        Scanner in = new Scanner(System.in);	
+       System.out.print("Input a positive integer: ");
+        int n = in.nextInt(); 
+		if (n>0)
+		{
+           System.out.print("The single digit number is: "+(n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9)));
+		}
+		System.out.println("\n");  
 
 	}
 
