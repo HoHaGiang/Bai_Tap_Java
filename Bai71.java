c72ca24657598354b4477b099b78b62d6b367b3a
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:32:50 +0700

    Update Bai71.java

--- Bai71.java
+++ Bai71.java
@@ -1,9 +1,16 @@
 package baitap;
-
+import java.lang.*;
 public class Bai71 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.lang.*;
+ 
+ public static void main(String[] args)
+ {
+    String str1 = "Python";    
+	String str2 = "Tutorial"; 
+	
+	System.out.println(str1.substring(1) + str2.substring(1));
 
 	}
 
