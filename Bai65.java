1ad27086e9b7dbdf907c0077c39380c91f29931d
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:28:29 +0700

    Update Bai65.java

--- Bai65.java
+++ Bai65.java
@@ -1,9 +1,19 @@
 package baitap;
-
+import java.util.*;
 public class Bai65 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+ public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int a = in.nextInt();  
+		System.out.print("Input the second number: ");
+		int b = in.nextInt(); 
+		int divided = a / b;
+		int result = a - (divided * b);
+		System.out.println(result); 
 
 	}
 
