5e92a35b844f52d661f9a00589776e2e7f6c9eb0
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:26:57 +0700

    Update Bai63.java

--- Bai63.java
+++ Bai63.java
@@ -1,9 +1,26 @@
 package baitap;
-
+import java.util.*;
 public class Bai63 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.util.*;
+
+ public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int a = in.nextInt();  
+		System.out.print("Input the second number: ");
+		int b = in.nextInt(); 
+		System.out.println("Result: "+result(a, b));
+    }
+	public static int result(int x, int y)
+     {  
+	if(x == y)
+		return 0;
+	    if(x % 6 == y % 6)
+		    return (x < y) ? x : y;
+	    return (x > y) ? x : y;
 
 	}
 
