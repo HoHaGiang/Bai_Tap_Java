81b947f2595266e64b87e16734239f8bf7137db9
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:45:10 +0700

    Update Bai89.java

--- Bai89.java
+++ Bai89.java
@@ -1,9 +1,14 @@
 package baitap;
-
+import java.lang.*;
+ 
 public class Bai89 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+ public static void main(String[] args)
+ {
+	System.out.println("System security interface:");
+    System.out.println(System.getSecurityManager());
 
 	}
 
