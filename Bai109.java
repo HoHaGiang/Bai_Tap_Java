5b7f6f38026a9ca8f029d64af5d68ce076bed651
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:16:25 +0700

    Update Bai109.java

--- Bai109.java
+++ Bai109.java
@@ -1,9 +1,18 @@
 package baitap;
-
+import java.util.Scanner;
 public class Bai109 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+     public static void main(String[] arg) {
+	  Scanner in = new Scanner(System.in);	
+       System.out.print("Input a positive integer: ");
+        int n = in.nextInt(); 
+		if (n>0)
+		{
+           System.out.println("Number of rows: "+((int)((Math.sqrt(8 * (long)n + 1) - 1) / 2)));	
+		}       	
 
 	}
 
