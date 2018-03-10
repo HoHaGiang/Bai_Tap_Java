9a5d08508e1c1afe5199a305da42ecd2a7525d13
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:32:22 +0700

    Update Bai70.java

--- Bai70.java
+++ Bai70.java
@@ -1,9 +1,18 @@
 package baitap;
-
+import java.lang.*;
 public class Bai70 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+ public static void main(String[] args)
+ {
+    String str1 = "Python";    
+	String str2 = "Tutorial"; 
+	
+	if(str1.length() >= str2.length())
+		System.out.println( str2+str1+str2);
+	else
+	System.out.println(str1+str2+str1);	
 
 	}
 
