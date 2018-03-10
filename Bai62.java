e273f3c3e2c4b0bdcc4af0a3176b4efcefa4e593
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:25:59 +0700

    Update Bai62.java

--- Bai62.java
+++ Bai62.java
@@ -1,9 +1,20 @@
 package baitap;
-
+mport java.util.*;
 public class Bai62 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+ 
+ public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int x = in.nextInt();  
+		System.out.print("Input the second number: ");
+		int y = in.nextInt(); 
+		System.out.print("Input the third number : ");
+        int z = in.nextInt(); 
+        System.out.println((Math.abs(x - y) >= 20 || Math.abs(y - z) >= 20 || Math.abs(z - x) >= 20));
 
 	}
 
