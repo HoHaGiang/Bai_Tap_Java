c263f7931b371e5148767f94f40076f721f87436
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:31:45 +0700

    Update Bai69.java

--- Bai69.java
+++ Bai69.java
@@ -1,9 +1,19 @@
 package baitap;
-
+import java.lang.*;
 public class Bai69 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.lang.*;
+ 
+ public static void main(String[] args)
+ {
+    String str1 = "Python";    
+	String str2 = "Tutorial"; 
+	
+	if(str1.length() >= str2.length())
+		System.out.println( str2+str1+str2);
+	else
+	System.out.println(str1+str2+str1);	
 
 	}
 
