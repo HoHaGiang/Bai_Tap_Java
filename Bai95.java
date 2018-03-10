32fc26cbb9284bed851d605d630f7814e3e37633
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:49:29 +0700

    Update Bai95.java

--- Bai95.java
+++ Bai95.java
@@ -1,9 +1,19 @@
 package baitap;
-
+import java.io.*; 
 public class Bai95 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		import java.util.*; 
+
+ 
+ public static void main(String[] args)
+ {
+    int n= 5;
+	String[] arr_string = new String[n];
+	
+    for(int i = 0; i < n; i++)
+        arr_string[i] = String.valueOf(i);                                  
+	 System.out.println("New Array: "+Arrays.toString(arr_string)); 
 
 	}
 
