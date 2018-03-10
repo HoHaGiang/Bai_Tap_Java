ba7c63fce33de5591b3d3d8a9ef1e33d2009a709
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:09:25 +0700

    Update Bai101.java

--- Bai101.java
+++ Bai101.java
@@ -1,10 +1,30 @@
 package baitap;
-
+import java.io.*; 
+ 
 public class Bai101 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		import java.util.*; 
+import java.io.*; 
+ 
+ public static void main(String[] args)
+ {
+	int[] array_nums = {10, 11, 10, 30, 45, 20, 33, 53};
+    int result = 0; 
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+    
+	int ctr1 = 0;
+    int ctr2 = 0;
+      
+    for(int i = 0; i < array_nums.length; i++) {
+        if(array_nums[i] == 10)
+            ctr1++;
+                        
+        if(array_nums[i] == 20)
+            ctr2++;
+    }                                      
+    System.out.printf(String.valueOf(ctr1 > ctr2));
+	System.out.printf("\n");
 	}
 
 }
