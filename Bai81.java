a5e4dcfacde71c637ece85613cdfe1c7c99b26a3
HoHaGiang <37130694+HoHaGiang@users.noreply.github.com>
2018-03-10 08:39:24 +0700

    Update Bai81.java

--- Bai81.java
+++ Bai81.java
@@ -1,9 +1,17 @@
 package baitap;
-
+import java.util.Arrays; 
 public class Bai81 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+ public static void main(String[] args)
+ {
+    int[] array_nums = {20, 30, 40};
+	System.out.println("Original Array: "+Arrays.toString(array_nums)); 
+	int x = array_nums[0];
+	array_nums[0] = array_nums[array_nums.length-1];
+	array_nums[array_nums.length-1] = x;
+	System.out.println("New array after swaping the first and last elements: "+Arrays.toString(array_nums));  
 
 	}
 
